484728aac1323ddead31f75b7d8e7fefab3f41d1
timdown <timdown@340d80fa-eec5-11dd-a0d4-8f5d5d5a31c6>
2009-02-06 01:10:58 +0000

    Update to docs and batch file

--- tools/shrink.bat
+++ tools/shrink.bat
@@ -1,6 +1,6 @@
-java -jar shrinksafe.jar ../hashtable.js > ../dist/jshashtable_temp.js
-jsmin <../dist/jshashtable_temp.js >../dist/jshashtable.js
-delete ../dist/jshashtable_temp.js
-copy ../hashtable.js ../dist/jshashtable_src.js
-copy ../license.txt ../dist
-copy ../docs ../dist
+java -jar shrinksafe.jar ..\hashtable.js > ..\dist\jshashtable_temp.js
+jsmin <..\dist\jshashtable_temp.js >..\dist\jshashtable.js
+del ..\dist\jshashtable_temp.js
+xcopy ..\hashtable.js ..\dist\jshashtable_src.js
+xcopy ..\license.txt ..\dist
+xcopy ..\docs ..\dist
